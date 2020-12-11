8da71d37b37334eff0e4dcc1755996bd9b35e76c
kmranrg <kmranrg@gmail.com>
2020-12-11 18:54:01 +0530

    added oop introduction

---
+++ 01_Hello_World/source.dart
@@ -0,0 +1,15 @@
+/// Let's start the learning with a simple Hello World Program
+
+main() {
+  print("OUTPUT:");
+  print("Hello World");
+}
+
+/*
+NOTE:
+
+1) The main() function is a predefined method in Dart.
+2) This method acts as the entry point to the application.
+3) A Dart script needs the main() method for execution.
+4) print() is a predefined function that prints the specified string or value to the standard output i.e. the terminal.
+*/
---
+++ 04_Object_Oriented_Introduction/source.dart
@@ -0,0 +1,19 @@
+/// Object-Oriented Programming in Dart
+
+/*
+THEORY:
+
+1) Dart is an Object-Oriented language.
+2) Object Orientation considers a program as a collection of objects that communicate with each other via mechanism called methods.
+*/
+
+class MyClass {
+  void display() {
+    print("Hello, I am Anurag.");
+  }
+}
+
+void main() {
+  MyClass obj = new MyClass();
+  obj.display();
+}

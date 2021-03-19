c56689e7e5d55333089ae6c2642ca39f4d978852
kmranrg <kmranrg@gmail.com>
2021-03-19 19:21:45 +0530

    symbols in dart

---
+++ 21_Symbol/Anurag.dart
@@ -0,0 +1,25 @@
+library anurag_lib; // the name of the library can be a Symbol
+
+class KumarAnurag {
+  // class name can also be Symbol
+  m1() {
+    // method name can be a Symbol too
+    print("Inside m1");
+  }
+
+  m2() {
+    // method name can be a Symbol too
+    print("Inside m2");
+  }
+
+  m3() {
+    // method name can be a Symbol too
+    print("Inside m3");
+  }
+}
+
+class Pooja {
+  p1() {
+    print("Inside p1");
+  }
+}

1fdbc249b6a822b5ecec3ee1dd4d2327560e3dd9
Kumar Anurag <kmranrg@gmail.com>
2021-06-13 13:33:25 +0530

    updated bitwise not operator

--- 10_BitwiseOperators/source.dart
+++ 10_BitwiseOperators/source.dart
@@ -8,7 +8,7 @@ Assumption:`a` and `b` are 1 bit numbers in binary form whereas `x` and `y` are 
 1) Bitwise AND (a & b): returns true(1) if both a and b are true(1) otherwise false(0)
 2) Bitwise OR (a | b): returns true(1) if any of a and b is true(1) otherwise false(0)
 3) Bitwise XOR (a ^ b): returns true(1) only if a and b are different [means true(1)-false(0) or false(0)-true(1)] otherwise false(0)
-4) Bitwise NOT (~a): inverts the bits of its operand (0 -> 1 and 1 -> 0)
+4) Bitwise NOT (~x): adds one to x and then multiply it with -1 and returns the final answer ( i.e. -(x+1) )
 5) Left Shift (x << y): it is equivalent of [ x * (2 raise to power y) ]
 6) Signpropagating Right Shift (x << y): it is equivalent of [ x / (2 raise to power y) ]. Note: In final result, it ignores the after decimal values
 */
